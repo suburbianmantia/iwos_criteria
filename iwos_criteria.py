df76c23f530a9fbf829a0256596ac7a8100e1a1d
suburbianmantia <74857304+suburbianmantia@users.noreply.github.com>
2024-05-05 17:02:52 +0200

    Update iwos_criteria.py

--- iwos_criteria.py
+++ iwos_criteria.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 
 st.title("Simple implementation of revised IWOS criteria for sarcoid uveitis classification")
-st.markdown("Here is a free implementation of the sarcoid uveitis classification according to modified IWOS criteria \n*Mochizuki M, Smith JR, Takase H for the International Workshop on Ocular Sarcoidosis Study Group, et al \nRevised criteria of International Workshop on Ocular Sarcoidosis (IWOS) for the diagnosis of ocular sarcoidosis\nBritish Journal of Ophthalmology 2019;103:1418-1422.*)")
+st.markdown("Here is a free implementation of the sarcoid uveitis classification according to modified IWOS criteria \n*Mochizuki M, Smith JR, Takase H for the International Workshop on Ocular Sarcoidosis Study Group, et al \nRevised criteria of International Workshop on Ocular Sarcoidosis (IWOS) for the diagnosis of ocular sarcoidosis \n British Journal of Ophthalmology 2019;103:1418-1422.*")
 
 st.subheader("I. Other causes of granulomatous uveitis must be ruled out")
 st.subheader("II. Intraocular signs suggestive of ocular sarcoidosis")
